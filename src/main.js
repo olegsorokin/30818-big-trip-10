5879bdb252be88fa032850d1087158ab45f70929
Oleg Sorokin <oleg.sorokin@rtbid.ru>
2019-12-05 20:54:33 +0600

    Added the create event function to main.js

--- src/main.js
+++ src/main.js
@@ -6,26 +6,28 @@ import {createFormCardCreateTemplate} from './components/card-create';
 import {createFormCardEditTemplate} from './components/card-edit';
 import {createBoardTemplate} from './components/board';
 import {createCardTemplate} from './components/card';
+import {createTripEvents} from './mock/trip-event';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const CARD_COUNT = 3;
+const tripEvents = createTripEvents(CARD_COUNT);
 
 const tripInfo = document.querySelector(`.trip-info`);
 const tripControls = document.querySelector(`.trip-controls`);
 const menuTitle = document.querySelector(`.trip-controls .visually-hidden:first-child`);
-const tripEvents = document.querySelector(`.trip-events`);
+const pageTripEvents = document.querySelector(`.trip-events`);
 
 render(tripInfo, createInfoTemplate(), `afterbegin`);
 render(menuTitle, createMenuTemplate(), `afterend`);
 render(tripControls, createFiltersTemplate(), `beforeend`);
-render(tripEvents, createSortTemplate(), `beforeend`);
-render(tripEvents, createFormCardCreateTemplate(), `beforeend`);
-render(tripEvents, createFormCardEditTemplate(), `beforeend`);
-render(tripEvents, createBoardTemplate(), `beforeend`);
+render(pageTripEvents, createSortTemplate(), `beforeend`);
+render(pageTripEvents, createFormCardCreateTemplate(), `beforeend`);
+render(pageTripEvents, createFormCardEditTemplate(), `beforeend`);
+render(pageTripEvents, createBoardTemplate(), `beforeend`);
 
 const cards = document.querySelector(`.trip-events__list`);
 
-new Array(CARD_COUNT).fill(``).forEach(() => render(cards, createCardTemplate(), `beforeend`));
+tripEvents.forEach((tripEvent) => render(cards, createCardTemplate(tripEvent), `beforeend`));
